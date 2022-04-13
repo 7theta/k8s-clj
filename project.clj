0c01c5e690523aceec20f905fe823b0038693625
Achint Sandhu <sandhu@7theta.com>
2022-04-13 12:13:38 -0400

    Pull in aleph via via

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject com.7theta/k8s "0.1.2"
   :description "A clojure library to make interaction with the K8s REST API available in code"
   :url "https://github.com/7theta/k8s-clj"
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [com.7theta/aleph "0.4.7-alpha9-3"]
-                 [metosin/jsonista "0.3.2"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.7theta/via "10.2.1"]
+                 [metosin/jsonista "0.3.5"]
                  [inflections "0.13.2"]
                  [stencil "0.5.0"]]
   :repl-options {:init-ns k8s.core})
