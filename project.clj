2114620a16ae73f20b1be1980760c69c2d9fdf38
C.T. Paterson <ct@7theta.com>
2022-04-13 11:15:48 -0400

    Switch httpkit for aleph

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.7theta/k8s "0.1.1"
+(defproject com.7theta/k8s "0.1.2"
   :description "A clojure library to make interaction with the K8s REST API available in code"
   :url "https://github.com/7theta/k8s-clj"
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [http-kit "2.5.3"]
+                 [com.7theta/aleph "0.4.7-alpha9-3"]
                  [metosin/jsonista "0.3.2"]
                  [inflections "0.13.2"]
                  [stencil "0.5.0"]]
